138e0e9aea21bb8a36c1196140efe053d2fecfb8
Jeff Brennan <jeffbrennan10@gmail.com>
2024-09-16 13:03:28 -0400

    update schemas - add pks

--- ampere/models.py
+++ ampere/models.py
@@ -2,21 +2,23 @@ import datetime
 from dataclasses import dataclass
 from typing import Optional, TypeVar
 
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Field
 
 SQLModelType = TypeVar("SQLModelType", bound=SQLModel)
 
 
 class Stargazer(SQLModel):
     __tablename__ = "stargazers"  # pyright: ignore [reportAssignmentType]
-    user_id: int
+    repo_id: int = Field(primary_key=True, foreign_key="repo.repo_id")
+    user_id: int = Field(primary_key=True, foreign_key="user.user_id")
     starred_at: datetime.datetime
     retrieved_at: datetime.datetime
 
 
 class Watcher(SQLModel):
     __tablename__ = "watchers"  # pyright: ignore [reportAssignmentType]
-    user_id: int
+    repo_id: int = Field(primary_key=True, foreign_key="repo.repo_id")
+    user_id: int = Field(primary_key=True, foreign_key="user.user_id")
     watched_at: datetime.datetime
     retrieved_at: datetime.datetime
 
@@ -27,10 +29,9 @@ class Language:
     size_bytes: int
 
 
-
 class Repo(SQLModel):
     __tablename__ = "repos"  # pyright: ignore [reportAssignmentType]
-    repo_id: int
+    repo_id: int = Field(primary_key=True)
     repo_name: str
     license: Optional[str] = None
     topics: list[str]
@@ -48,8 +49,9 @@ class Repo(SQLModel):
 
 class Fork(SQLModel):
     __tablename__ = "forks"  # pyright: ignore [reportAssignmentType]
+    repo_id: int = Field(primary_key=True, foreign_key="repo.repo_id")
+    fork_id: int = Field(primary_key=True)
     owner_id: int
-    fork_id: int
     retrieved_at: datetime.datetime
 
 
@@ -57,6 +59,8 @@ class Fork(SQLModel):
 # https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28#get-a-commit
 class Commit(SQLModel):
     __tablename__ = "commits"  # pyright: ignore [reportAssignmentType]
+    repo_id: int = Field(primary_key=True, foreign_key="repo.repo_id")
+    commit_id: str = Field(primary_key=True)
     author_id: int
     comment_count: int
     message: str
@@ -69,7 +73,8 @@ class Commit(SQLModel):
 # https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#list-repository-issues
 class Issue(SQLModel):
     __tablename__ = "issues"  # pyright: ignore [reportAssignmentType]
-    issue_id: int
+    repo_id: int = Field(primary_key=True, foreign_key="repo.repo_id")
+    issue_id: int = Field(primary_key=True)
     issue_number: int
     issue_title: str
     issue_body: str
@@ -86,7 +91,7 @@ class Issue(SQLModel):
 # https://docs.github.com/en/rest/metrics/traffic?apiVersion=2022-11-28#get-page-views
 class View(SQLModel):
     __tablename__ = "views"  # pyright: ignore [reportAssignmentType]
-    repo_id: int
+    repo_id: int = Field(primary_key=True, foreign_key="repo.repo_id")
     view_count: int
     unique_view_count: int
     view_date: datetime.date
@@ -96,7 +101,8 @@ class View(SQLModel):
 # https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests
 class PullRequest(SQLModel):
     __tablename__ = "pull_requests"  # pyright: ignore [reportAssignmentType]
-    pr_id: int
+    repo_id: int = Field(primary_key=True, foreign_key="repo.repo_id")
+    pr_id: int = Field(primary_key=True)
     pr_number: int
     pr_title: str
     pr_state: str
@@ -112,7 +118,7 @@ class PullRequest(SQLModel):
 # https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#get-a-user
 class User(SQLModel):
     __tablename__ = "users"  # pyright: ignore [reportAssignmentType]
-    user_id: int
+    user_id: int = Field(primary_key=True)
     user_name: str
     full_name: str
     company: str
@@ -128,13 +134,11 @@ class User(SQLModel):
 # https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#list-releases
 class Release(SQLModel):
     __tablename__ = "releases"  # pyright: ignore [reportAssignmentType]
-    release_id: int
+    repo_id: int = Field(primary_key=True, foreign_key="repo.repo_id")
+    release_id: int = Field(primary_key=True)
     release_name: str
     tag_name: str
-    release_body: str
-    release_state: str
-    release_size: int
-    download_count: int
+    release_body: Optional[str] = None
     created_at: datetime.datetime
     published_at: datetime.datetime
     retrieved_at: datetime.datetime
