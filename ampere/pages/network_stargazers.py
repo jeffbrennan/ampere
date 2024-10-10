c9ef83c04d0c1108f14299b147813b076c5863ab
Jeff Brennan <jeffbrennan10@gmail.com>
2024-10-09 22:23:12 -0400

    add stargazer network graph

--- ampere/pages/network_stargazers.py
+++ ampere/pages/network_stargazers.py
@@ -1,13 +1,38 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash import html
+from dash import dcc, callback, Output, Input
+from plotly.graph_objects import Figure
 
-from .side_bar import sidebar
+from ampere.pages.side_bar import sidebar
+from ampere.viz import viz_star_network
 
 dash.register_page(__name__, name="network", top_nav=True, order=1)
 
 
 def layout(**kwargs):
     return dbc.Row(
-        [dbc.Col(sidebar(), width=2), dbc.Col(html.Div("Stargazers"), width=10)]
+        [
+            dbc.Col(sidebar(), width=1),
+            dcc.Interval(
+                id="network-follower-load-interval",
+                n_intervals=0,
+                max_intervals=0,
+                interval=1,
+            ),
+            dbc.Col(
+                dcc.Graph(
+                    id="network-follower-graph",
+                    style={"height": "100vh"},
+                ),
+                width=11,
+            ),
+        ]
     )
+
+
+@callback(
+    Output("network-follower-graph", "figure"),
+    Input("network-follower-load-interval", "n_intervals"),
+)
+def show_summary_graph(_: int) -> Figure:
+    return viz_star_network()
