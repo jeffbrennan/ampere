7dfd9e48504d96dc3896ff2b573b70fb11ce39e6
Jeff Brennan <jeffbrennan10@gmail.com>
2024-10-11 00:45:27 -0400

    add last updated footer that matches top navbar

--- ampere/app.py
+++ ampere/app.py
@@ -1,12 +1,30 @@
+import datetime
 import dash
-
 import dash_bootstrap_components as dbc
+
+from ampere.common import get_db_con
+from dash import dcc, html
 
 app = dash.Dash(
     __name__,
     use_pages=True,
     external_stylesheets=[dbc.themes.BOOTSTRAP],
 )
+
+
+def get_last_updated() -> datetime.datetime:
+    con = get_db_con()
+    max_retrieved_at = (
+        con.sql("SELECT max(retrieved_at) as last_updated FROM main.repos")
+        .to_df()
+        .squeeze()
+    )
+
+    return max_retrieved_at
+
+
+last_updated = get_last_updated()
+last_updated_str = last_updated.strftime("%Y-%m-%d")
 
 navbar = dbc.NavbarSimple(
     dbc.Nav(
@@ -22,8 +40,19 @@ navbar = dbc.NavbarSimple(
     className="mb-2",
 )
 
+footer = html.Footer(
+    children=[html.Div(f"last updated {last_updated_str}", style={"textAlign": "left"})],
+    style={
+        "position": "fixed",
+        "bottom": "0",
+        "width": "100%",
+        "backgroundColor": "#316CF4",
+        "color": "#FFFFFF",
+        "paddingLeft": "4.5vw",
+    },
+)
 app.layout = dbc.Container(
-    [navbar, dash.page_container],
+    [navbar, dash.page_container, footer],
     fluid=True,
 )
 
