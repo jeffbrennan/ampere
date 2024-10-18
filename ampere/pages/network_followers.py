429720fcef9666ab2435ff9c7f72b4e7304d857c
Jeff Brennan <jeffbrennan10@gmail.com>
2024-10-18 15:37:26 -0400

    format table

--- ampere/pages/network_followers.py
+++ ampere/pages/network_followers.py
@@ -12,21 +12,22 @@ dash.register_page(__name__)
 
 def create_followers_table() -> pd.DataFrame:
     con = get_db_con()
-    return con.sql(
+    df = con.sql(
         """
         SELECT
             concat('[', user_name, ']', '(https://www.github.com/', user_name, ')')  user_name,
             full_name as name,
             followers_count as followers,
-            following_count as following,
             internal_followers_count as "org followers",
+            round(internal_followers_pct * 100.0, 2) as "org followers %",
+            following_count as following,
             internal_following_count as "org following",
-            internal_followers_pct as "org followers %",
-            internal_following_pct as "org following %"
-        FROM int_network_follower_details 
+            round(internal_following_pct * 100.0, 2) as "org following %"
+        FROM int_network_follower_details
         order by followers_count desc
         """
     ).to_df()
+    return df
 
 
 def layout(**kwargs):
@@ -54,9 +55,11 @@ def layout(**kwargs):
         dash_table.DataTable(
             df.to_dict("records"),
             columns=[
-                {"id": x, "name": "", "presentation": "markdown"}
-                if x == "user_name"
-                else {"id": x, "name": x}
+                (
+                    {"id": x, "name": "", "presentation": "markdown"}
+                    if x == "user_name"
+                    else {"id": x, "name": x}
+                )
                 for x in df.columns
             ],
             id="tbl",
@@ -72,18 +75,21 @@ def layout(**kwargs):
                 "backgroundColor": "#3F6DF9",
                 "padding": "10px",
                 "color": "#FFFFFF",
+                "fontWeight": "bold",
             },
             style_cell={
-                "textAlign": "center",
+                "textAlign": "right",
                 "minWidth": 95,
                 "maxWidth": 95,
                 "width": 95,
-                "font_size": "14px",
+                "font_size": "1em",
                 "whiteSpace": "normal",
                 "height": "auto",
+                "font-family": "sans-serif",
             },
+            css=[dict(selector="p", rule="margin-bottom: 0; text-align: right;")],
             style_table={
-                "height": "500px",
+                "height": "50%",
                 "overflowY": "scroll",
                 "overflowX": "scroll",
                 "margin": {"b": 100},
