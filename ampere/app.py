c610eedc1cf6c1ab6a83c7cc14fc6b62d06058ab
Jeff Brennan <jeffbrennan10@gmail.com>
2024-10-20 03:47:52 -0400

    replace header and footer with single navigation bar

--- ampere/app.py
+++ ampere/app.py
@@ -1,16 +1,22 @@
 import datetime
+from enum import StrEnum
 
 import dash
 import dash_bootstrap_components as dbc
-from dash import html, callback, Input, Output
+from dash import html, Input, Output, State
 
 from ampere.common import get_db_con
 
 app = dash.Dash(
-    __name__,
     use_pages=True,
     external_stylesheets=[dbc.themes.BOOTSTRAP],
 )
+
+
+class Palette(StrEnum):
+    PAGE_ACCENT_COLOR = "#3F6DF9"
+    BRAND_TEXT_COLOR = "#FFFFFF"
+    BRAND_TEXT_COLOR_MUTED = "#CEE5F2"
 
 
 def get_last_updated() -> datetime.datetime:
@@ -26,52 +32,80 @@ def get_last_updated() -> datetime.datetime:
 
 last_updated = get_last_updated()
 last_updated_str = last_updated.strftime("%Y-%m-%d")
-
-navbar = dbc.NavbarSimple(
-    children=[
-        dbc.DropdownMenu(
-            children=[
-                dbc.DropdownMenuItem("stargazers", href="network-stargazers"),
-                dbc.DropdownMenuItem("followers", href="network-followers"),
-            ],
-            nav=True,
-            in_navbar=True,
-            label="networks",
+page_links_collapsed = dbc.Row(
+    [
+        dbc.Col(
+            dbc.DropdownMenu(
+                children=[
+                    dbc.DropdownMenuItem("stargazers", href="network-stargazers"),
+                    dbc.DropdownMenuItem("followers", href="network-followers"),
+                ],
+                label="networks",
+                toggle_style={
+                    "color": Palette.BRAND_TEXT_COLOR_MUTED,
+                    "backgroundColor": Palette.PAGE_ACCENT_COLOR,
+                    "borderColor": Palette.PAGE_ACCENT_COLOR,
+                },
+                class_name="navbar-text",
+            ),
         ),
-        dbc.NavItem(dbc.NavLink("about", href="about")),
+        dbc.Col(
+            dbc.NavItem(
+                dbc.NavLink(
+                    "about",
+                    href="about",
+                    style={"color": Palette.BRAND_TEXT_COLOR_MUTED},
+                    class_name="navbar-text",
+                )
+            ),
+        ),
     ],
-    brand="ampere",
-    brand_href="/",
-    color="#3F6DF9",
-    className="mb-2",
-    dark=True,
-    fluid=True,
-    links_left=True,
-    style={
-        "width": "100%",
-        "color": "#FFFFFF",
-    },
+    align="left",
+    class_name="row align-items-center",
 )
 
-footer = html.Footer(
-    children=[
-        html.Div(
-            f"last updated {last_updated_str}",
-            style={"textAlign": "left", "fontSize": "14px"},
-        )
-    ],
-    style={
-        "position": "fixed",
-        "bottom": "0",
-        "width": "100%",
-        "backgroundColor": "#3F6DF9",
-        "color": "#FFFFFF",
-        "paddingLeft": "1%",
-        "left": "0",
-    },
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                dbc.Row([dbc.NavbarBrand("ampere")]),
+                href="/",
+                style={"textDecoration": "none"},
+            ),
+            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+            dbc.Collapse(
+                page_links_collapsed,
+                id="navbar-collapse",
+                is_open=False,
+                navbar=True,
+            ),
+            html.P(
+                f"last updated {last_updated_str}",
+                style={"marginBottom": "0", "color": Palette.BRAND_TEXT_COLOR_MUTED},
+            ),
+        ],
+        fluid=True,
+    ),
+    color=Palette.PAGE_ACCENT_COLOR,
+    dark=True,
+    sticky="top",
 )
+
+
+# add callback for toggling the collapse on small screens
+@app.callback(
+    Output("navbar-collapse", "is_open"),
+    [Input("navbar-toggler", "n_clicks")],
+    [State("navbar-collapse", "is_open")],
+)
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+
 app.layout = dbc.Container(
-    [navbar, dash.page_container, footer],
+    [navbar, dash.page_container],
     fluid=True,
     style={
         "paddingLeft": "5%",
