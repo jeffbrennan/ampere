1abcdd57c64c0be65ddd0087c24c59011e2ecb7d
Jeff Brennan <jeffbrennan10@gmail.com>
2024-10-27 19:39:29 -0400

    move date spine to helper model

---
+++ models/helpers/helper_date_spine.sql
@@ -0,0 +1,17 @@
+with
+    date_spine as (
+        {{
+            dbt_utils.date_spine(
+                datepart="day",
+                start_date="(select min(metric_timestamp) from "
+                + ref("int_repo_metrics")
+                | string + ")",
+                end_date="(select max(metric_timestamp) from "
+                + ref("int_repo_metrics")
+                | string + ")",
+            )
+        }}
+    ),
+    date_spine_clean as (select cast(date_day as date) as spine_date from date_spine)
+    select *
+    from date_spine_clean
