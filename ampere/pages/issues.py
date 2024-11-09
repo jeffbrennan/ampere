fc727d7f4dd2499b55fbb0bb7ddaf421b23b65b6
Jeff Brennan <jeffbrennan10@gmail.com>
2024-11-08 21:30:35 -0500

    move issues and issues summary tables to dbt models

--- ampere/pages/issues.py
+++ ampere/pages/issues.py
@@ -18,25 +18,17 @@ dash.register_page(__name__, name="feed", top_nav=True, order=3)
 def create_issues_table() -> pd.DataFrame:
     con = get_db_con()
     return con.sql(
-        """        
-        select
-            concat('[', repo_name, ']', '(https://www.github.com/mrpowers-io/', repo_name, ')') as "repo",
-            concat('[', user_name, ']', '(https://www.github.com/', user_name, ')')             as "author",
-            concat('[', a.issue_title, ']',
-                   '(https://github.com/mrpowers-io/', c.repo_name,
-                   '/issues/', a.issue_number, ')'
-            )                                                                                   as "title",
-            coalesce(a.issue_body, '')                                                          as "body",
-            strftime(a.created_at, '%Y-%m-%d')                                                  as "date",
-            date_part('day', current_date - a.created_at)                                       as "days old",
-            a.comments_count                                                                    as "comments"
-        from issues a
-        join users b
-             on a.author_id = b.user_id
-        join repos c
-             on a.repo_id = c.repo_id
-        where a.state = 'open'
-        order by repo_name, a.created_at
+        """
+       select
+           repo,
+           author,
+           title,
+           body,
+           "date",
+           "days old",
+           "comments"
+       from mart_issues
+       order by repo, "days old"
         """
     ).to_df()
 
@@ -44,62 +36,14 @@ def create_issues_table() -> pd.DataFrame:
 def create_issues_summary_table() -> pd.DataFrame:
     con = get_db_con()
     return con.sql(
-        """with
-    closed_issues_past_month as (
-        select
-            repo_id,
-            count(issue_id) as closed_issues
-        from issues
-        where
-            state = 'closed'
-            and closed_at >= current_date - 120
-        group by repo_id
-    ),
-    open_issues_base as (
-        select
-            repo_id,
-            issue_id,
-            date_part('day', current_date - created_at) as age_days
-        from issues
-        where
-            state = 'open'
-    ),
-    open_issues as (
-        select
-            repo_id,
-            count(issue_id) as open_issues_count,
-            avg(age_days)   as avg_age_days
-        from open_issues_base
-        group by repo_id
-    ),
-    new_issues as (
-        select repo_id,
-        count(issue_id) as new_issues_count,
-        from issues
-        where state = 'open'
-        and created_at >= current_date - 120
-        group by repo_id
-    ),
-    repo_spine as (
-        select
-            repo_id,
-            repo_name
-        from repos
-    )
-select
-    concat('[', a.repo_name, ']', '(https://www.github.com/mrpowers-io/', a.repo_name, ')') as "repo",
-    coalesce(b.open_issues_count, 0) as "open issues",
-    ceil(coalesce(b.avg_age_days, 0))     as "avg issue age (days)",
-    coalesce(d.new_issues_count, 0) as "new issues (this month)",
-    coalesce(c.closed_issues, 0)     as "closed issues (this month)",
-from repo_spine a
-left join open_issues b
-    on a.repo_id = b.repo_id
-left join closed_issues_past_month c
-    on a.repo_id = c.repo_id
-left join new_issues d
-on a.repo_id = d.repo_id
-    order by open_issues_count desc
+        """
+        select repo,
+            "open issues",
+            "avg issue age (days)",
+            "new issues (this month)",
+            "closed issues (this month)"
+        from mart_issues_summary
+        order by "open issues" desc
         """
     ).to_df()
 
