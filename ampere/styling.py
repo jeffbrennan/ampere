a0015597404a9cb6ce7c030958281f90aa9307e0
Jeff Brennan <jeffbrennan10@gmail.com>
2024-11-08 20:31:09 -0500

    add table titles, apply consistent margin rules, add loading bars to reduce first-time load jank

--- ampere/styling.py
+++ ampere/styling.py
@@ -58,9 +58,11 @@ def style_dt_background_colors_by_rank(
 
     for col in cols:
         colors = colorlover.scales[str(n_bins)]["seq"][col.palette]
-        ranks = df[col.name].rank(ascending=col.ascending, method="max").astype("int") - 1
-        ranks = ranks.squeeze().tolist()
-        print(ranks)
+        ranks = (
+            (df[col.name].rank(ascending=col.ascending, method="max").astype("int") - 1)
+            .squeeze()
+            .tolist() # type: ignore
+        )
         for row in range(n_bins):
             row_val = df[col.name].iloc[row]
             rank_val = ranks[row]
@@ -138,3 +140,15 @@ AmpereDTStyle = asdict(
         },
     )
 )
+
+table_title_style = {
+    "color": AmperePalette.BRAND_TEXT_COLOR,
+    "backgroundColor": AmperePalette.PAGE_ACCENT_COLOR,
+    "paddingBottom": "0",
+    "paddingLeft": "10px",
+    "paddingRight": "10px",
+    "fontSize": "1.5rem",
+    "fontWeight": "bold",
+    "marginBottom": "0",
+    "border": "none",
+}
