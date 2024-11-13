d68383e8df8310a906b71592143630ea22a24fdf
Jeff Brennan <jeffbrennan10@gmail.com>
2024-11-13 14:37:06 -0500

    make backfill generic, taking optional min date or using existing org repo creation date

--- scripts/backfill_pypi_downloads.py
+++ scripts/backfill_pypi_downloads.py
@@ -1,15 +1,26 @@
+from typing import Optional
+
 from ampere.common import get_db_con
 from ampere.get_pypi_downloads import add_backfill_to_table
+import datetime
+
+def backfill_repo(repo: str, min_date: Optional[datetime.datetime], repo_dependency: Optional[str], dry_run: bool):
+    if min_date is None and repo_dependency is None:
+        raise ValueError(
+            "expecting either `min_date` or `repo_dependency` to be provided"
+        )
 
 
-def backfill_pyspark():
-    con = get_db_con()
-    min_date = con.sql(
-        "select created_at from repos where repo_name = 'quinn'"
-    ).fetchall()[0][0]
-    print(min_date)
-    add_backfill_to_table("pyspark", min_date, True)
+    if min_date is None and repo_dependency is not None:
+        con = get_db_con()
+        min_date = con.sql(
+            f"select created_at from repos where repo_name = '{repo_dependency}'"
+        ).fetchall()[0][0]
+
+    if min_date is None:
+        raise ValueError("expecting min date to be set")
+    add_backfill_to_table(repo, min_date, dry_run)
 
 
 if __name__ == "__main__":
-    backfill_pyspark()
+    backfill_repo("deltalake", None, 'levi', False)
