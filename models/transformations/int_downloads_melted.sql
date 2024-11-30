2f04744bba357a7438dfdc02b37e251ee8b29b39
Jeff Brennan <jeffbrennan10@gmail.com>
2024-11-30 00:12:29 -0600

    add incremental transformations to decrease daily runtimes (~5 min -> 3 sec)

--- models/transformations/int_downloads_melted.sql
+++ models/transformations/int_downloads_melted.sql
@@ -1,13 +1,85 @@
-with
-melted as ({{
-    dbt_utils.unpivot(
-        ref('stg_downloads'),
-        exclude=['project','timestamp', 'download_count'],
-        remove=['retrieved_at'],
-        field_name='group_name',
-        value_name='group_value'
+{{
+    config(
+        materialized='incremental',
+        unique_key=[
+          'repo',
+          'download_timestamp',
+          'group_name',
+          'group_value'
+        ]
     )
-  }}
+}}
+
+
+with
+base as (
+    select * from {{ ref('stg_downloads_inc') }}
+    {% if is_incremental() %}
+        where
+            timestamp
+            > (
+                select coalesce(max(download_timestamp), '1900-01-01') --noqa
+                from {{ this }}
+            )
+    {% endif %}
+),
+
+melted as (
+    select
+        project,
+        timestamp,
+        'country_code' as group_name,
+        country_code as group_value,
+        download_count
+    from base
+    union all
+    select
+        project,
+        timestamp,
+        'package_version' as group_name,
+        package_version as group_value,
+        download_count
+    from base
+    union all
+    select
+        project,
+        timestamp,
+        'python_version' as group_name,
+        python_version as group_value,
+        download_count
+    from base
+    union all
+    select
+        project,
+        timestamp,
+        'system_distro_name' as group_name,
+        system_distro_name as group_value,
+        download_count
+    from base
+    union all
+    select
+        project,
+        timestamp,
+        'system_distro_version' as group_name,
+        system_distro_version as group_value,
+        download_count
+    from base
+    union all
+    select
+        project,
+        timestamp,
+        'system_name' as group_name,
+        system_name as group_value,
+        download_count
+    from base
+    union all
+    select
+        project,
+        timestamp,
+        'system_release' as group_name,
+        system_release as group_value,
+        download_count
+    from base
 )
 
 select
