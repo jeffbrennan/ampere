1a53b0adaedeec047119d5100ee2ee93005f07fb
Jeff Brennan <jeffbrennan10@gmail.com>
2024-12-01 15:58:24 -0600

    add test for python version parsing

---
+++ data-tests/assert_int_downloads_summary_python_version_parsed_correctly.sql
@@ -0,0 +1,18 @@
+with base as (
+    select
+        group_name,
+        group_value,
+        regexp_matches(group_value, '^\d{1}\.\d{1,2}$|^unknown$') as is_valid
+    from
+        {{ ref('int_downloads_summary') }}
+    where group_name = 'python_version'
+)
+
+select
+    group_value,
+    is_valid,
+    count(*) as n
+from base
+where not is_valid
+group by all
+order by n desc
