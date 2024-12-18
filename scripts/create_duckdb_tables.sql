105790af9faf78300f7b843830cc7965b9ea2208
Jeff Brennan <jeffbrennan10@gmail.com>
2024-12-18 18:55:53 +0000

    tables w/ absolute paths -> views with relative paths

--- scripts/create_duckdb_tables.sql
+++ scripts/create_duckdb_tables.sql
@@ -1,47 +1,47 @@
-create table commits as
+create view if not exists commits as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/commits");
+from delta_scan("../data/bronze/commits");
 
-create table forks as
+create view if not exists forks as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/forks");
+from delta_scan("../data/bronze/forks");
 
-create table issues as
+create view if not exists issues as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/issues");
+from delta_scan("../data/bronze/issues");
 
-create table pull_requests as
+create view if not exists pull_requests as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/pull_requests");
+from delta_scan("../data/bronze/pull_requests");
 
-create table releases as
+create view if not exists releases as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/releases");
+from delta_scan("../data/bronze/releases");
 
-create table repos as
+create view if not exists repos as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/repos");
+from delta_scan("../data/bronze/repos");
 
-create table stargazers as
+create view if not exists stargazers as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/stargazers");
+from delta_scan("../data/bronze/stargazers");
 
-create table users as
+create view if not exists users as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/users");
+from delta_scan("../data/bronze/users");
 
-create table watchers as
+create view if not exists watchers as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/watchers");
+from delta_scan("../data/bronze/watchers");
 
-create table followers as
+create view if not exists followers as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/followers");
+from delta_scan("../data/bronze/followers");
 
-create table pypi_downloads as
+create view if not exists pypi_downloads as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/pypi_downloads");
+from delta_scan("../data/bronze/pypi_downloads");
 
-create table pypi_download_queries as
+create view if not exists pypi_download_queries as
 select *
-from delta_scan("/Users/jeffb/Desktop/Life/personal-projects/ampere/data/bronze/pypi_download_queries");
+from delta_scan("../data/bronze/pypi_download_queries");
