0d4165e9401b72b5660aaa77dba4f90d26120649
Jeff Brennan <jeffbrennan10@gmail.com>
2024-12-18 19:28:34 +0000

    update paths and document usage

--- scripts/create_duckdb_tables.sql
+++ scripts/create_duckdb_tables.sql
@@ -1,47 +1,48 @@
+-- usage: to be copied into ampere/data/ folder and executed from there
 create view if not exists commits as
 select *
-from delta_scan("../data/bronze/commits");
+from delta_scan("../../ampere/data/bronze/commits");
 
 create view if not exists forks as
 select *
-from delta_scan("../data/bronze/forks");
+from delta_scan("../../ampere/data/bronze/forks");
 
 create view if not exists issues as
 select *
-from delta_scan("../data/bronze/issues");
+from delta_scan("../../ampere/data/bronze/issues");
 
 create view if not exists pull_requests as
 select *
-from delta_scan("../data/bronze/pull_requests");
+from delta_scan("../../ampere/data/bronze/pull_requests");
 
 create view if not exists releases as
 select *
-from delta_scan("../data/bronze/releases");
+from delta_scan("../../ampere/data/bronze/releases");
 
 create view if not exists repos as
 select *
-from delta_scan("../data/bronze/repos");
+from delta_scan("../../ampere/data/bronze/repos");
 
 create view if not exists stargazers as
 select *
-from delta_scan("../data/bronze/stargazers");
+from delta_scan("../../ampere/data/bronze/stargazers");
 
 create view if not exists users as
 select *
-from delta_scan("../data/bronze/users");
+from delta_scan("../../ampere/data/bronze/users");
 
 create view if not exists watchers as
 select *
-from delta_scan("../data/bronze/watchers");
+from delta_scan("../../ampere/data/bronze/watchers");
 
 create view if not exists followers as
 select *
-from delta_scan("../data/bronze/followers");
+from delta_scan("../../ampere/data/bronze/followers");
 
 create view if not exists pypi_downloads as
 select *
-from delta_scan("../data/bronze/pypi_downloads");
+from delta_scan("../../ampere/data/bronze/pypi_downloads");
 
 create view if not exists pypi_download_queries as
 select *
-from delta_scan("../data/bronze/pypi_download_queries");
+from delta_scan("../../ampere/data/bronze/pypi_download_queries");
