2f53c7be8f992e3da690512d1685280856f7b7d4
Jeff Brennan <jeffbrennan10@gmail.com>
2024-12-17 22:43:56 -0500

    update schedule

--- ampere/dagster/schedules.py
+++ ampere/dagster/schedules.py
@@ -3,7 +3,7 @@ from dagster import AssetSelection, DefaultScheduleStatus, ScheduleDefinition
 bigquery_schedule = ScheduleDefinition(
     name="bigquery_schedule",
     target=AssetSelection.groups("bigquery_daily"),
-    cron_schedule="0 8 * * *",  # daily 8 am
+    cron_schedule="0 12 * * *",  # daily 7 am et
     default_status=DefaultScheduleStatus.RUNNING,
 )
 schedules = [bigquery_schedule]
