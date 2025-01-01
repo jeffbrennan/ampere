61fd2064c176271ba62f6ae708cc880540159426
Jeff Brennan <jeffbrennan10@gmail.com>
2025-01-01 05:44:48 +0000

    update stale hours

--- models/staging/stg_followers.sql
+++ models/staging/stg_followers.sql
@@ -8,7 +8,7 @@ with base as (
         retrieved_at,
         row_number() over (partition by user_id, follower_id order by retrieved_at desc) as rn
     from  {{source('main', 'followers')}}
-    where retrieved_at >= (select max(retrieved_at) - interval 8 hours from {{source('main', 'followers')}})
+    where retrieved_at >= (select max(retrieved_at) - interval 24 hours from {{source('main', 'followers')}})
 )
 select
     user_id,
