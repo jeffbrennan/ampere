4fb5dddddd4c0b08ceebcf067a14212bffb3aab0
Jeff Brennan <jeffbrennan10@gmail.com>
2025-01-04 04:26:56 +0000

    use smaller datatype for metric count (max val = 65k)

--- models/marts/mart_repo_summary.sql
+++ models/marts/mart_repo_summary.sql
@@ -2,7 +2,7 @@ select
     b.repo_name,
     a.metric_type,
     a.metric_date,
-    a.metric_count::bigint as metric_count
+    a.metric_count::usmallint as metric_count
 from
     {{ ref("int_repo_metrics_filled") }} as a
 left join {{ref("stg_repos")}} as b
