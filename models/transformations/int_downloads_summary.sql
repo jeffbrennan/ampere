e8b7a6cb37eebad775bcc0a88991becc6ceab083
Jeff Brennan <jeffbrennan10@gmail.com>
2025-01-08 00:36:41 +0000

    add pyspark, deltalake to downloads page, restrict number of groups in package versions visualization to 10

--- models/transformations/int_downloads_summary.sql
+++ models/transformations/int_downloads_summary.sql
@@ -8,8 +8,7 @@ unknown_python as (
         download_count
     from {{ ref('int_downloads_melted_weekly') }}
     where
-        repo not in ('pyspark', 'deltalake')
-        and group_name = 'python_version'
+        group_name = 'python_version'
         and group_value = 'unknown'
 ),
 
@@ -26,8 +25,7 @@ python_major_minor as (
         sum(download_count) as download_count
     from {{ ref('int_downloads_melted_weekly') }}
     where
-        repo not in ('pyspark', 'deltalake')
-        and group_name = 'python_version'
+        group_name = 'python_version'
         and group_value != 'unknown'
     group by all
 ),
@@ -46,11 +44,11 @@ clouds as (
         end as group_value,
         sum(download_count) as download_count
     from {{ ref('int_downloads_melted_weekly') }}
-    where repo not in ('pyspark', 'deltalake') and group_name = 'system_release'
+    where group_name = 'system_release'
     group by all
 ),
 
-package_versions as (
+package_versions_raw as (
     select
         repo,
         download_date,
@@ -58,8 +56,46 @@ package_versions as (
         group_value,
         download_count
     from {{ ref('int_downloads_melted_weekly') }}
-    where
-        repo not in ('pyspark', 'deltalake') and group_name = 'package_version'
+    where group_name = 'package_version'
+),
+
+recent_package_version_counts as (
+select
+    repo,
+    group_name,
+    group_value,
+    sum(download_count) as download_count
+    from package_versions_raw
+    where download_date >= (select max(download_date) - interval 30 day from package_versions_raw)
+    group by all
+),
+
+recent_package_versions_ranked as (
+select
+    repo,
+    group_name,
+    group_value,
+    row_number() over (partition by repo, group_name
+    order by download_count desc) as rn
+    from recent_package_version_counts
+),
+
+recent_package_versions_top_ten as (
+    select * from recent_package_versions_ranked
+    where rn <= 10
+),
+
+package_versions as  (
+    select
+        a.repo,
+        a.download_date,
+        a.group_name,
+        case when b.group_value is null then 'other' else a.group_value end as group_value,
+        sum(a.download_count) as download_count
+    from package_versions_raw as a
+    left join recent_package_versions_top_ten as b
+    on a.repo = b.repo and a.group_name = b.group_name and a.group_value = b.group_value
+    group by all
 ),
 
 operating_systems as (
@@ -70,7 +106,7 @@ operating_systems as (
         group_value,
         download_count
     from {{ ref('int_downloads_melted_weekly') }}
-    where repo not in ('pyspark', 'deltalake') and group_name = 'system_name'
+    where group_name = 'system_name'
 ),
 
 overall as (
@@ -81,7 +117,7 @@ overall as (
         'overall' as group_value,
         sum(download_count) as download_count
     from {{ ref('int_downloads_melted_weekly') }}
-    where repo not in ('pyspark', 'deltalake') and group_name = 'system_name'
+    where group_name = 'system_name'
     group by all
 ),
 
