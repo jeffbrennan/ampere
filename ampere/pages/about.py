3145dde29728b1bf0446d805e1a397c11ec14cf6
Jeff Brennan <jeffbrennan10@gmail.com>
2025-01-16 00:20:02 -0500

    update margins, font sizes for about page

--- ampere/pages/about.py
+++ ampere/pages/about.py
@@ -3,7 +3,7 @@ import pandas as pd
 from dash import Input, Output, callback, dash_table, html
 
 from ampere.common import get_frontend_db_con
-from ampere.styling import get_ampere_dt_style
+from ampere.styling import ScreenWidth, get_ampere_dt_style
 
 
 def create_repo_table() -> pd.DataFrame:
@@ -31,28 +31,50 @@ def create_repo_table() -> pd.DataFrame:
         Output("about-text", "children"),
         Output("about-text", "style"),
     ],
-    Input("color-mode-switch", "value"),
+    [
+        Input("color-mode-switch", "value"),
+        Input("breakpoints", "widthBreakpoint"),
+    ],
 )
-def get_styled_about_text(dark_mode: bool):
+def get_styled_about_text(dark_mode: bool, breakpoint_name: str):
     color = "white" if dark_mode else "black"
+    if breakpoint_name in [ScreenWidth.xs, ScreenWidth.sm]:
+        font_size = "12px"
+        margin_left = "0vw"
+    else:
+        font_size = "16px"
+        margin_left = "12vw"
+
     return [
         html.Div(
             id="about-text",
             children=[
-                html.Div("made with ❤️ by ", style={"display": "inline", "color": color}),
+                html.Div(
+                    "made with ❤️ by ",
+                    style={"display": "inline", "color": color, "fontSize": font_size},
+                ),
                 html.A(
                     "Jeff Brennan",
                     href="https://github.com/jeffbrennan",
                     target="_blank",
+                    style={"fontSize": font_size},
                 ),
-                html.Div(" and ", style={"display": "inline", "color": color}),
+                html.Div(
+                    " and ",
+                    style={"display": "inline", "color": color, "fontSize": font_size},
+                ),
                 html.A(
                     "mrpowers-io",
                     href="https://github.com/mrpowers-io",
                     target="_blank",
+                    style={"fontSize": font_size},
                 ),
-                html.Div(" contributors", style={"display": "inline", "color": color}),
+                html.Div(
+                    " contributors",
+                    style={"display": "inline", "color": color, "fontSize": font_size},
+                ),
             ],
+            style={"marginLeft": margin_left},
         ),
     ], {}
 
@@ -63,14 +85,27 @@ def get_styled_about_text(dark_mode: bool):
         Output("about-table", "style"),
         Output("about-fade", "is_in"),
     ],
-    Input("color-mode-switch", "value"),
+    [
+        Input("color-mode-switch", "value"),
+        Input("breakpoints", "widthBreakpoint"),
+    ],
 )
-def get_styled_about_table(dark_mode: bool):
+def get_styled_about_table(dark_mode: bool, breakpoint_name: str):
     df = create_repo_table()
     about_style = get_ampere_dt_style(dark_mode)
-    about_style["style_table"]["maxHeight"] = "37vh"
-    about_style["style_table"]["height"] = "37vh"
+    about_style["style_table"]["height"] = "auto"
 
+    sm_margins = {"maxWidth": "90vw", "width": "90vw"}
+    lg_margins = {"maxWidth": "65vw", "width": "65vw", "marginLeft": "12vw"}
+
+    if breakpoint_name in [ScreenWidth.xs, ScreenWidth.sm]:
+        about_style["style_cell"]["font_size"] = "12px"
+        about_style["style_table"].update(sm_margins)
+
+    else:
+        about_style["style_table"].update(lg_margins)
+
+    # print(about_style)
     tbl = dash_table.DataTable(
         df.to_dict("records"),
         columns=[
