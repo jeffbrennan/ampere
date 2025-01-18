0568301a8aef66ca7b0c6afbe871143f34f3d259
Jeff Brennan <jeffbrennan10@gmail.com>
2025-01-18 15:51:47 -0500

    add max timestamp test

---
+++ data-tests/assert_int_downloads_summary_weekly_not_exceed_current_date.sql
@@ -0,0 +1,14 @@
+with dates as (
+    select
+        group_name, 
+        max(download_date) as max_timestamp
+    from int_downloads_melted_weekly
+    group by all
+)
+select
+    group_name,
+    max_timestamp,
+    now() as current_timestamp,
+    (epoch(max_timestamp) - epoch(current_timestamp)) / 3600 as diff_hours
+from dates
+where max_timestamp > now()
