016beccf583baed4e6f0f2d999f06be1155b29ef
Jeff Brennan <jeffbrennan10@gmail.com>
2025-01-23 00:27:59 -0500

    add table output

--- ampere/cli/commands/downloads.py
+++ ampere/cli/commands/downloads.py
@@ -1,11 +1,77 @@
+import requests
 import typer
+from rich.console import Console
+from rich.table import Table
+
+from ampere.api.models import DownloadPublic, DownloadsPublic
+from ampere.api.routes.downloads import (
+    DownloadsGranularity,
+    DownloadsPublicGroup,
+    GetDownloadsPublicConfig,
+    RepoEnum,
+)
+from ampere.cli.common import get_api_url
+from ampere.cli.state import State
+from ampere.common import timeit
+
+console = Console()
 
 downloads_app = typer.Typer()
 
 
+def format_downloads_list_output(response: DownloadsPublic):
+    table = Table("Repo", "Timestamp", "Group Name", "Group Value", "Download Count")
+    for item in response.data:
+        table.add_row(
+            item.repo,
+            item.download_timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+            item.group_name,
+            item.group_value,
+            str(item.download_count),
+        )
+
+    return table
+
+
+def get_downloads_response(config: GetDownloadsPublicConfig) -> DownloadsPublic:
+    base_url = get_api_url(State.env)
+    url = f"{base_url}/downloads/{config.granularity}"
+    response = requests.get(
+        url,
+        params={
+            "repo": config.repo,
+            "group": config.group,
+            "n_days": config.n_days,
+            "limit": config.limit,
+            "descending": config.descending,
+        },
+    )
+    return DownloadsPublic.model_validate(response.json())
+
+
 @downloads_app.command("list")
-def list_downloads():
-    print("List of downloads")
+@timeit
+def list_downloads(
+    granularity: DownloadsGranularity,
+    repo: RepoEnum,  # type: ignore
+    group: DownloadsPublicGroup = DownloadsPublicGroup.overall,
+    n_days: int = 30,
+    limit: int = 50,
+    descending: bool = True,
+):
+    response = get_downloads_response(
+        GetDownloadsPublicConfig(
+            granularity=granularity,
+            repo=repo,
+            group=group,
+            n_days=n_days,
+            limit=limit,
+            descending=descending,
+        )
+    )
+
+    table = format_downloads_list_output(response)
+    console.print(table)
 
 
 @downloads_app.command("summary")
