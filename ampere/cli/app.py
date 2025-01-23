550238ab78a48f7d6870f67b247f15856280186a
Jeff Brennan <jeffbrennan10@gmail.com>
2025-01-23 00:28:23 -0500

    add cli state to pass global environment flag for local testing

--- ampere/cli/app.py
+++ ampere/cli/app.py
@@ -2,10 +2,16 @@ import typer
 
 from ampere.cli.commands.downloads import downloads_app
 from ampere.cli.commands.feed import feed_app
+from ampere.cli.common import CLIEnvironment
+from ampere.cli.state import State
 
 app = typer.Typer()
 app.add_typer(downloads_app, name="downloads")
 app.add_typer(feed_app, name="feed")
+
+@app.callback()
+def main(env: CLIEnvironment = CLIEnvironment.prod):
+    State.env = env
 
 
 if __name__ == "__main__":
