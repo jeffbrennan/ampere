29c731d602a24019d41f1233096182aea48fdd88
Jeff Brennan <jeffbrennan10@gmail.com>
2025-01-23 21:38:33 -0500

    update table styling

--- ampere/cli/commands/downloads.py
+++ ampere/cli/commands/downloads.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 from concurrent.futures import ThreadPoolExecutor
 from enum import StrEnum, auto
@@ -6,6 +7,7 @@ from typing import Annotated, Optional
 import requests
 import typer
 from pydantic import BaseModel
+from rich import box
 from rich.console import Console
 from rich.table import Table
 
@@ -17,7 +19,7 @@ from ampere.api.routes.downloads import (
     GetDownloadsPublicConfig,
     RepoEnum,
 )
-from ampere.cli.common import CLIEnvironment, get_api_url
+from ampere.cli.common import CLIEnvironment, get_api_url, get_flag_emoji
 from ampere.cli.models import CLIOutputFormat
 from ampere.cli.state import State
 from ampere.common import timeit
@@ -30,6 +32,8 @@ downloads_app = typer.Typer()
 class DownloadsSummary(BaseModel):
     granularity: DownloadsSummaryGranularity
     repo: RepoEnum  # type: ignore
+    min_date: datetime.datetime
+    max_date: datetime.datetime
     last_period: int
     this_period: int
     pct_change: float
@@ -40,8 +44,22 @@ def format_downloads_summary_output(
     granularity: DownloadsSummaryGranularity,
     descending: bool,
 ) -> Table:
-    period = granularity.name.capitalize().removesuffix("ly")
-    table = Table("Repo", f"Last {period}", f"This {period}", "% Change")
+    period = granularity.name.removesuffix("ly")
+    min_date = min([record.min_date for record in records])
+    max_date = max([record.max_date for record in records])
+
+    title = f"Downloads Summary"
+    subtitle = f"{min_date.strftime('%Y-%m-%d')} to {max_date.strftime('%Y-%m-%d')}"
+    title_full = f"{title}\n{subtitle}"
+
+    table = Table(
+        title=title_full, title_justify="left", title_style="bold", box=box.ROUNDED
+    )
+    table.add_column("repo", justify="left")
+    table.add_column(f"last {period}", justify="right")
+    table.add_column(f"this {period}", justify="right")
+    table.add_column("% change", justify="right")
+
     records = sorted(records, key=lambda x: x.pct_change, reverse=descending)
     for record in records:
         table.add_row(
@@ -54,21 +72,51 @@ def format_downloads_summary_output(
     return table
 
 
-def format_downloads_list_output(response: DownloadsPublic) -> Table:
-    table = Table("Repo", "Timestamp", "Group Name", "Group Value", "Download Count")
-    for item in response.data:
-        table.add_row(
-            item.repo,
-            item.download_timestamp.strftime("%Y-%m-%d %H:%M:%S"),
-            item.group_name,
-            item.group_value,
-            str(item.download_count),
-        )
+def format_downloads_list_output(response: DownloadsPublic, descending: bool) -> Table:
+    repo = response.data[0].repo
+    group = response.data[0].group_name
 
+    title = f"{repo} {group} downloads"
+    table = Table(
+        title=title,
+        title_justify="left",
+        title_style="bold",
+        box=box.ROUNDED,
+    )
+    table.add_column("timestamp", justify="center")
+    table.add_column("group value", justify="left")
+    table.add_column("downloads", justify="right")
+    table.add_column("%", justify="right")
+
+    totals_by_timestamp = {}
+    for item in response.data:
+        if item.download_timestamp not in totals_by_timestamp:
+            totals_by_timestamp[item.download_timestamp] = 0
+        totals_by_timestamp[item.download_timestamp] += item.download_count
+
+    prev_timestamp = response.data[0].download_timestamp
+    for item in response.data:
+        if item.download_timestamp != prev_timestamp:
+            table.add_section()
+        prev_timestamp = item.download_timestamp
+
+        pct = item.download_count / totals_by_timestamp[item.download_timestamp] * 100
+
+        if group == "country_code":
+            group_emoji = get_flag_emoji(item.group_value)
+            group_value = f"{group_emoji} {item.group_value}"
+        else:
+            group_value = item.group_value
+
+        table.add_row(
+            item.download_timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+            group_value,
+            str(item.download_count),
+            f"{pct:.2f}%",
+        )
     return table
 
 
-@timeit
 def get_downloads_response(config: GetDownloadsPublicConfig) -> DownloadsPublic:
     if State.env == CLIEnvironment.dev:
         time.sleep(0.4)
@@ -90,7 +138,6 @@ def get_downloads_response(config: GetDownloadsPublicConfig) -> DownloadsPublic:
 
 
 @downloads_app.command("list")
-@timeit
 def list_downloads(
     granularity: Annotated[
         DownloadsGranularity,
@@ -128,7 +175,7 @@ def list_downloads(
         console.print_json(response.model_dump_json())
         return
 
-    table = format_downloads_list_output(response)
+    table = format_downloads_list_output(response, descending)
     console.print(table)
 
 
@@ -137,6 +184,8 @@ def create_downloads_summary(
 ) -> list[DownloadsSummary]:
     summary = []
     for record in records:
+        min_date = min([item.download_timestamp for item in record.data])
+        max_date = max([item.download_timestamp for item in record.data])
         last_period = record.data[-2].download_count
         this_period = record.data[-1].download_count
         pct_change = (this_period - last_period) / last_period * 100
@@ -147,6 +196,8 @@ def create_downloads_summary(
                 last_period=last_period,
                 this_period=this_period,
                 pct_change=pct_change,
+                min_date=min_date,
+                max_date=max_date,
             )
         )
 
@@ -154,7 +205,6 @@ def create_downloads_summary(
 
 
 @downloads_app.command("summary")
-@timeit
 def summarize_downloads(
     granularity: Annotated[
         DownloadsSummaryGranularity,
