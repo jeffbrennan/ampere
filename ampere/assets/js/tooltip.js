483cdfae710d4d24fd0c98a8e647dfc8835d23dd
Jeff Brennan <jeffbrennan10@gmail.com>
2025-01-29 22:32:03 +0000

    fix seconds to ymd conversion to use utc

--- ampere/assets/js/tooltip.js
+++ ampere/assets/js/tooltip.js
@@ -1,8 +1,8 @@
 window.dccFunctions = window.dccFunctions || {};
 window.dccFunctions.secondsToYMD = function(value) {
     const date = new Date(value * 1000);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(date.getUTCDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
 }
