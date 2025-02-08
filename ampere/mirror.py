7a48a8627c4d4d9152f129023fdd43103cb628cb
Jeff Brennan <jeffbrennan10@gmail.com>
2025-02-08 18:34:01 +0000

    copy in batches of rows to limit memory usage

--- ampere/mirror.py
+++ ampere/mirror.py
@@ -39,9 +39,29 @@ def write_backend_tables_to_frontend() -> None:
     backend_con = get_backend_db_con()
     frontend_con = get_frontend_db_con(read_only=False)
     for table in tables:
-        print(f"writing {table}...")
-        df = backend_con.sql(f"SELECT * FROM {table}").to_df()
-        duckdb.sql(f"CREATE TABLE {table} AS SELECT * FROM df", connection=frontend_con)
+        offset = 0
+        batch_size = 200_000
+        first_batch = True
+        while True:
+            print(f"writing {table} records {offset:,} to {(offset + batch_size):,}...")
+
+            df = backend_con.sql(
+                f"SELECT * FROM {table} LIMIT {batch_size} OFFSET {offset}"
+            ).to_df()
+
+            if df.empty:
+                break
+
+            if first_batch:
+                duckdb.sql(
+                    f"CREATE TABLE {table} AS SELECT * FROM df", connection=frontend_con
+                )
+                first_batch = False
+            else:
+                duckdb.sql(
+                    f"INSERT INTO {table} SELECT * FROM df", connection=frontend_con
+                )
+            offset += batch_size
 
 
 def write_backend_views_to_frontend() -> None:
