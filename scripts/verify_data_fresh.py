88f3b6df5aa1dbe69dd20550e51c70263aa73f6e
Jeff Brennan <jeffbrennan10@gmail.com>
2025-05-22 10:21:38 -0400

    add script for cronjob

---
+++ scripts/verify_data_fresh.py
@@ -0,0 +1,63 @@
+from ampere.common import get_frontend_db_con, get_secret
+from dotenv import load_dotenv
+import smtplib
+
+
+def send_email(body: str) -> None:
+    load_dotenv()
+    subject = "[ALERT] Ampere Data Stale"
+
+    sender = get_secret("AMPERE_BACKEND_EMAIL_FROM")
+    password = get_secret("AMPERE_BACKEND_EMAIL_PW")
+    recipient = get_secret("AMPERE_BACKEND_EMAIL_LIST")
+
+    headers = [
+        f"To: {recipient}",
+        f"From: {sender}",
+        f"Subject: {subject}",
+        "MIME-Version: 1.0",
+        "Content-Type: text/html",
+    ]
+
+    headers_str = "\r\n".join(headers)
+    content = headers_str + "\r\n\r\n" + body
+    print(content)
+
+    mail = smtplib.SMTP("smtp.gmail.com", 587)
+    mail.ehlo()
+    mail.starttls()
+
+    mail.login(sender, password)
+    mail.sendmail(from_addr=sender, to_addrs=recipient, msg=content)
+
+    mail.quit()
+
+
+def check_freshness() -> None:
+    con = get_frontend_db_con()
+    results = (
+        con.sql("""
+            select *
+            from int_status_summary
+            where stale = false
+        """)
+        .to_df()
+        .to_dict(orient="records")
+    )
+
+    if len(results) == 0:
+        print("data is fresh")
+        return
+
+    formatted_content = "<br>".join(
+        [
+            f"STALE: {result['page']} [{result['hours_stale']} hours stale]"
+            for result in results
+        ]
+    )
+
+    send_email(formatted_content)
+
+
+if __name__ == "__main__":
+    check_freshness()
